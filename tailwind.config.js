9aae4399768ed3dfe0a586d8280725a62d29794a
Iris Huang <lyaui@outloook.com>
2021-11-23 01:36:18 +0800

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: '#CBE5FE',
+          dark: '#b2d8fe',
+        },
+        green: {
+          DEFAULT: '#D4EDDA',
+          dark: '#c2e5cb',
+        },
+      },
+    },
   },
   variants: {
     extend: {},

6938024c1c70482caec054c8b1008d94cab75987
Iris Huang <lyaui@outloook.com>
2021-11-23 02:06:54 +0800

    import TaskInput & TaskList into Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import TaskInput from 'components/UI/TaskInput.js';
+import TaskList from 'components/Task/TaskList.js';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className='text-3xl mb-6'>TODO List</h1>
+      <TaskInput />
+      <TaskList />
+    </div>
+  );
 };
 
 export default Home;

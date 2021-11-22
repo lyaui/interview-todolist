d39cb37cbcfd055ce65e44bfc50ec98d19700505
Iris Huang <lyaui@outloook.com>
2021-11-22 23:57:54 +0800

    add Layout style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ROUTES } from 'constants/routes.js';
+import Layout from 'components/Layout';
 import Home from 'pages/Home.js';
 import Management from 'pages/Management.js';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path={ROUTES.HOME} element={<Home />} />
-      <Route path={ROUTES.MANAGEMENT} element={<Management />} />
-      <Route path='/*' element={<Navigate replace to={ROUTES.HOME} />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.MANAGEMENT} element={<Management />} />
+        <Route path='/*' element={<Navigate replace to={ROUTES.HOME} />} />
+      </Routes>
+    </Layout>
   );
 };
 
---
+++ src/components/Layout/index.js
@@ -0,0 +1,5 @@
+const Layout = ({ children }) => {
+  return <div className='bg-gray-50 h-screen px-16 py-10'>{children}</div>;
+};
+
+export default Layout;

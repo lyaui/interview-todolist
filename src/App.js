e83a388ff153d6a635350ae3e706df893205f270
Iris Huang <lyaui@outloook.com>
2021-11-22 22:00:32 +0800

    add page routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { ROUTES } from 'constants/routes.js';
+import Home from 'pages/Home.js';
+import Management from 'pages/Management.js';
+
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
+    <Routes>
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.MANAGEMENT} element={<Management />} />
+      <Route path='/*' element={<Navigate replace to={ROUTES.HOME} />} />
+    </Routes>
   );
 };
 
